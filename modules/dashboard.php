db5a6dae5af0dc9ef36a8e84e8bbf14484f27275
parthp <parth.np5@gmail.com>
2017-03-10 00:19:41 +0530

    - Dashboard

---
+++ modules/dashboard.php
@@ -0,0 +1,30 @@
+<div class="panel panel-warning">
+  <div class="panel-heading">
+    <h3 class="panel-title">Pending Attendance</h3>
+  </div>
+  <div class="panel-body">
+    <p><a href="#">Class DB of <strong>23/02/2017</strong></a></p>
+		<p><a href="#">Class Maths of <strong>24/02/2017</strong></a></p>
+  </div>
+</div>
+
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title">Today's Attendance</h3>
+  </div>
+  <div class="panel-body">
+    <p><a href="#">Class JT of <strong>Today's</strong></a></p>
+		<p><a href="#">Class DFS of <strong>Today's</strong></a></p>
+  </div>
+</div>
+
+<div class="panel panel-success">
+  <div class="panel-heading">
+    <h3 class="panel-title">You have</h3>
+  </div>
+  <div class="panel-body">
+    <p><i class="fa fa-book"></i> <a href="/class/list"><strong>2</strong> Subjects</a></p>
+		<p><i class="fa fa-users"></i> <a href="/student/list"><strong>65</strong> Students</a></p>
+  </div>
+</div>
+
