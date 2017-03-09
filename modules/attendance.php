61fe44fe588ea77c359d2efb7759cee1fa5168dc
panchal <parth.np5@gmail.com>
2017-03-09 23:59:10 +0530

    sec

--- modules/attendance.php
+++ modules/attendance.php
@@ -1,4 +1,29 @@
+<?php include 'config.php';?>
 <h1 class="page-header">Take Attendance</h1>  
+<form action="attendance.php" method="post">
+	<div class="form-group">
+		<label for="select" class="">Subject</label>
+		<?php
+			$sub=$conn->query('select * from subject ');
+			$rsub=$sub->fetchAll(PDO::FETCH_ASSOC);
+			echo"<select class='form-control'>";
+			for($i = 0; $i<count($rsub); $i++)
+			{
+				echo"<option value='". $rsub[$i]['id']."'>".$rsub[$i]['name']."</option>";
+			}
+			echo"</select>";
+		?>									
+	</div>
+
+	<div class="form-group input-group date" data-provide="datepicker">
+		<label for="select" class="">Date</label>
+		<input type="date" class="form-control">
+	</div>
+
+	<input type="submit" class="btn btn-default" name="sbt_stn" value="Load Student">
+</form>
+	<input type="submit" class="btn btn-primary" name="sbt_top" value="Save Attendance">
+
 <table class="table table-striped table-hover ">
 	<thead>
 		<tr>
@@ -7,12 +32,28 @@
 			<th>isPresent</th>
 		</tr>
 	</thead>
-	<tbody>
-		<tr>
-			<td>1</td>
-			<td>Column content</td>
-			<td><input type="checkbox" name="chk1" checked></td>
-		</tr>
-		
-	</tbody>
+
+	<?php
+	if(false)
+	{
+
+	}
+	else
+	{
+		$st=$conn->query('select * from student');
+		$r=$st->fetchAll(PDO::FETCH_ASSOC);
+		echo"<tbody>";
+		for($i = 0; $i<count($r); $i++)
+		{
+			echo"<tr>";
+				echo"<td>".$r[$i]['rollno']."</td>";
+				echo"<td>".$r[$i]['name']."</td>";
+				echo"<td><input type='checkbox' name='chbox[]' value='" . $r[$i]['sid'] . "'></td>";
+			echo"</tr>";
+		}
+		echo"</tbody>";
+	}
+	?>
 </table> 
+
+<input type="submit" class="btn btn-primary" name="sbt_top" value="Save Attendance">
