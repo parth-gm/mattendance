d028dc70b6eb6e125cc023948015d675c94a9276
panchal <parth.np5@gmail.com>
2017-03-10 10:55:57 +0530

    new one

--- modules/attendance.php
+++ modules/attendance.php
@@ -27,8 +27,14 @@
 
 	<input type="submit" class="btn btn-default" name="sbt_stn" value="Load Student">
 </form>
-	<input type="submit" class="btn btn-primary" name="sbt_top" value="Save Attendance">
+	
 
+
+<?php
+	if(isset($_GET['date']) && isset($_GET['subject'])) :
+
+?>
+<form action="index.php" method="post">
 <table class="table table-striped table-hover ">
 	<thead>
 		<tr>
@@ -39,9 +45,9 @@
 	</thead>
 
 	<?php
-	
 			
-		$st=$conn->query('select * from student');
+			$qu = "SELECT student.sid, student.name, student.rollno from student INNER JOIN student_subject WHERE student.sid = student_subject.sid AND student_subject.id  = {$_GET['subject']}";
+		$st=$conn->query($qu);
 		$r=$st->fetchAll(PDO::FETCH_ASSOC);
 		echo"<tbody>";
 		for($i = 0; $i<count($r); $i++)
@@ -49,6 +55,9 @@
 			echo"<tr>";
 				echo"<td>".$r[$i]['rollno']."</td>";
 				echo"<td>".$r[$i]['name']."</td>";
+				/*if ($_GET['chbox[]'] == $r[$i]['id']) {
+					echo"<option value='". $r[$i]['id']."' selected='selected'>".$r[$i]['name']."</option>";
+				}*/
 				echo"<td><input type='checkbox' name='chbox[]' value='" . $r[$i]['sid'] . "'></td>";
 			echo"</tr>";
 		}
@@ -57,4 +66,37 @@
 	?>
 </table> 
 
+<input type="hidden" name="saveData" value="1">
+<input type="hidden" name="date" value="<?php print isset($_GET['date']) ? $_GET['date'] : ''; ?>">
+<input type="hidden" name="subject" value="<?php print isset($_GET['subject']) ? $_GET['subject'] : ''; ?>">
 <input type="submit" class="btn btn-primary" name="sbt_top" value="Save Attendance">
+
+<?php endif;?>
+</form>
+<?php
+
+	if(isset($_POST['saveData']) ) {
+	
+		// prepare sql and bind parameters
+	    $stmtInsert = $conn->prepare("INSERT INTO attendance (sid, date, ispresent, uid, id) 
+	    VALUES (:sid, :date, :ispresent, :uid, :id)");
+	    $stmtInsert->bindParam(':sid', $sid);
+	    $stmtInsert->bindParam(':date', $date);
+	    $stmtInsert->bindParam(':ispresent', $ispresent);
+	    $stmtInsert->bindParam(':uid', $uid);
+	    $stmtInsert->bindParam(':id', $id);
+
+	    $sid = 3;
+	    $date = 664;
+	    $ispresent = 1;
+	    $uid = 3;
+	    $id = 1;
+	    $stmtInsert->execute();
+		
+	}
+	
+
+
+
+
+?>
