1544d8d98a665484e8c440bb9177d66fe59d3d11
Mrugesh Panchal <mrugesh.panchal@icrossing.com>
2017-03-29 16:44:38 +0530

    - Client side validation added for login form. Minor UI tweaks for login form

--- modules/footer.php
+++ modules/footer.php
@@ -1,6 +1,5 @@
 	</div>
-
-	<script src="js/jquery.min.js"></script>
+	
     <script src="js/bootstrap.min.js"></script>
     <script src="js/custom.js"></script>
   </body>
--- modules/login.php
+++ modules/login.php
@@ -1,26 +1,23 @@
-
 <div class="container">
-  
   <div class="row">
     <div class="col-md-6 col-md-offset-3 col-lg-6 text-center">
       <img src="images/mattendance_logo_small.png" alt="mAttendance">
-      <h4>Teacher's Section</h4>
+      <h3>Teacher's Section</h3>
     </div>
   </div>
   <div class="row">
     <div class="col-md-6 col-md-offset-3 col-lg-6">
-      <form class="form-horizontal" action="index.php" method="post">
+      <form class="form-horizontal" id="loginForm" action="index.php" method="post" data-toggle="validator">
         <div class="form-group">
           <label for="inputEmail3" class="control-label">Username</label>
-          <input type="text" class="form-control" id="inputEmail3" name="name" placeholder="Username">
+          <input type="text" class="form-control" id="inputEmail3" name="name" maxlength="16" placeholder="Username" required>
         </div>
         <div class="form-group">
           <label for="inputPassword3" class="control-label">Password</label>
-          <input type="password" class="form-control" id="inputPassword3"  name="pass" placeholder="Password">
+          <input type="password" class="form-control" id="inputPassword3"  name="pass" maxlength="16" placeholder="Password" required>
         </div>
-
         <div class="form-group">
-          <input type="submit" name="submit" class="btn btn-default" value="Sign in">
+          <input type="submit" name="submit" class="btn btn-info btn-block" value="Sign in">
         </div>
       </form>
     </div>
@@ -28,21 +25,35 @@
   <hr class="col-md-offset-3 col-md-6" />
   <div class="row">
     <div class="col-md-6 col-md-offset-3 col-lg-6 text-center">
-      <h4>Student's Section</h4>
+      <h3>Student's Section</h3>
     </div>
   </div>
   <div class="row">
     <div class="col-md-6 col-md-offset-3 col-lg-6">
-      <form class="form-horizontal" action="modules/studentdata.php" method="post">
+      <form class="form-horizontal" action="modules/studentdata.php" method="post" id="studentForm" data-toggle="validator">
         <div class="form-group">
           <label for="rollno" class="control-label">Roll No.</label>
-          <input type="text" class="form-control" id="rollno"  name="rollno" placeholder="Roll No.">
-           <input type="submit" name="ssubmit"  value=" GO ">
+          <input type="text" class="form-control" id="rollno" maxlength="6" name="rollno" placeholder="Roll No." required>
         </div>
+				
+				<div class="form-group">
+					<input type="submit" name="ssubmit" class="btn btn-success btn-block" value="GO">
+				</div>
       </form>
     </div>  
   </div>
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3 col-lg-6 text-center">
+			<p>&copy; 2017 mAttendance.</p>
+		</div>
+	</div>
+	
 </div>
+
+<script>
+	$('#loginForm').validator();
+	$('#studentForm').validator();
+</script>
 
 
     <?php
