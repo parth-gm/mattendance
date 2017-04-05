983421a3a2847d045dd14a2574c8ed80dfa35c53
Mrugesh Panchal <mrugesh.panchal@icrossing.com>
2017-04-05 20:38:37 +0530

    - Minor UI fix

--- modules/studentinfo.php
+++ modules/studentinfo.php
@@ -37,7 +37,7 @@
 				</div>
 			</div>
 			
-			<div class="panel panel-primary">
+			<div class="panel panel-info">
 				<div class="panel-heading">
 					<h3 class="panel-title">Students</h3>
 				</div>
