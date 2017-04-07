5681c9159e523ba805a41eb065f6439e1ea3e0a1
Mrugesh Panchal <mrugesh.panchal@icrossing.com>
2017-04-07 17:40:37 +0530

    - Website title changed

--- modules/header.php
+++ modules/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Bootstrap 101 Template</title>
+    <title>mAttendance | Attendance on the Go build for DDU</title>
     
     <link href="css/bootstrap.min.css" rel="stylesheet">   
 		<link href="css/style.css" rel="stylesheet">
