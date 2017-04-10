bb27baf95e3945ff9ce0b543e8fa5134aece8c0c
Mrugesh Panchal <mrugesh.panchal@icrossing.com>
2017-04-10 15:02:02 +0530

    - Content updates

--- modules/studentspage.php
+++ modules/studentspage.php
@@ -7,9 +7,17 @@
 	
 	<div class="row">
     <div class="col-md-12 col-lg-12">
-			<h2>Student's Page</h2>
+			<h3>Go Green Go Paperless</h3>
+			<img src="images/gogreen.jpg" class="img-responsive" alt="">
+			<p class="lead">New Initiative for hasselfree attendance taking. Attendance are made now simpler for Faculty as well as Students.</p>
 		</div>
 	</div>
 	
+	<div class="row">
+    <div class="col-md-12 col-lg-12">
+			<h3>Friends attendace are important!</h3>
+			<p>As our policy 75% attendace is compulsary. Please check your latest attendace records from "Student Section".</p>
+		</div>
+	</div>
 	
 </div>
--- modules/teacherspage.php
+++ modules/teacherspage.php
@@ -7,8 +7,31 @@
 	
 	<div class="row">
     <div class="col-md-12 col-lg-12">
-			<h2>Teacher's Page</h2>
+			<h3>Go Green Go Paperless</h3>
+			<img src="images/gogreen.jpg" class="img-responsive" alt="">
+			<p class="lead">New Initiative for hasselfree attendance taking. Attendance are made now simpler for Faculty as well as Students.</p>
 		</div>
 	</div>
 	
+	<div class="row">
+    <div class="col-md-12 col-lg-12">
+			<h3>mAttendace is Device independant?</h3>
+			<p>mAtttendace is Dynamic and Robust. It wokrs on across the devices covering Andoid, iOS, Windows, Blackberry. I works very fine your Tables/Phables as well.</p>
+		</div>
+	</div>
+	
+	<div class="row">
+    <div class="col-md-12 col-lg-12">
+			<h3>From where I could get login details for attendance taking?</h3>
+			<p>mAtttendace is Dynamic and Robust. Your HOD have access to create your account for attendance taking</p>
+		</div>
+	</div>
+	
+	<div class="row">
+    <div class="col-md-12 col-lg-12">
+			<h3>How Students and Subjects are assigned to me?</h3>
+			<p>mAtttendace is Dynamic and Robust. Your HOD will do the needful for you from his admin access.</p>
+		</div>
+	</div>
+		
 </div>
